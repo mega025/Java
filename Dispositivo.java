1512eadc8d4fd747b8db6badaf729df7c940db10
mega025 <xinjiepan123@gmail.com>
2024-10-09 14:23:38 +0200

    09/10/2024

--- Dispositivo.java
+++ Dispositivo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 abstract class Dispositivo {
     public String marca;
    public estados estado;
@@ -6,6 +8,17 @@ abstract class Dispositivo {
     this.estado = estados.apagado;
     this.marca = marca;
    }
+
+public String getMarca() {
+    return marca;
+}
+
+public void setMarca() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("ingresa la marca de tu celu" );
+    this.marca = scanner.nextLine();
+    scanner.close();
+}
 
 
 abstract void encender();
